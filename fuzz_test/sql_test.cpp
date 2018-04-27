3c4d9e2302ee16ea1a6b3b6c36ced13b2c35c8b3
cyy <cyyever@outlook.com>
2018-04-27 17:57:44 +0800

    add fuzzing tests

---
+++ fuzz_test/sql_test.cpp
@@ -0,0 +1,35 @@
+/*!
+ * \file face_bound_test.cpp
+ *
+ * \author cyy
+ * \date 2017-12-29
+ */
+
+#include "../hdr/mariadb_modern_cpp.hpp"
+
+static std::unique_ptr<mariadb::database> &get_database() {
+  static std::unique_ptr<mariadb::database> db;
+
+  if (!db) {
+    mariadb::mariadb_config config;
+    config.host = "127.0.0.1";
+    config.user = "mariadb_modern_cpp_test";
+    config.passwd = "123";
+
+    db = std::make_unique<mariadb::database>("mariadb_modern_cpp_test", config);
+  }
+  return db;
+}
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
+  if (Size == 0) {
+    return 0;
+  }
+
+  try {
+    (*get_database()) << std::string(reinterpret_cast<const char *>(Data),
+                                     Size);
+  } catch (const mariadb::mariadb_exception &) {
+  }
+  return 0; // Non-zero return values are reserved for future use.
+}
