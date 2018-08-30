2bdb75e49174cb9ed6b7bc4cb31916b1457a8cc6
cyy <cyyever@outlook.com>
2018-08-30 18:15:03 +0800

    disconnect mysql connection when transaction fail and add according test

--- test/transaction_test.cpp
+++ test/transaction_test.cpp
@@ -33,6 +33,29 @@ TEST_CASE("transaction") {
     test_db << "drop table mariadb_modern_cpp_test.tmp_table;";
   }
 
+  SUBCASE("roll_back with invalid sql") {
+    test_db << "CREATE TABLE IF NOT EXISTS mariadb_modern_cpp_test.tmp_table "
+               "(id BIGINT PRIMARY KEY AUTO_INCREMENT NOT NULL);";
+    test_db << "delete from mariadb_modern_cpp_test.tmp_table;";
+
+    bool has_exception = false;
+    try {
+      auto ctx = test_db.get_transaction_context();
+      test_db << "INSERT INTO tmp_table VALUES ();";
+      size_t cnt = 0;
+      test_db << "select count(*) from tmp_table;" >> cnt;
+      CHECK(cnt == 1);
+      test_db << "invalid sql";
+    } catch (...) {
+      has_exception = true;
+    }
+    REQUIRE(has_exception);
+    size_t cnt = 0;
+    test_db << "select count(*) from tmp_table;" >> cnt;
+    CHECK(cnt == 0);
+    test_db << "drop table mariadb_modern_cpp_test.tmp_table;";
+  }
+
   SUBCASE("commit") {
     test_db << "CREATE TABLE IF NOT EXISTS mariadb_modern_cpp_test.tmp_table "
                "(id BIGINT PRIMARY KEY AUTO_INCREMENT NOT NULL);";
